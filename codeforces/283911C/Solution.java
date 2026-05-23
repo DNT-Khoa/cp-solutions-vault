35e33c8130ae97016032df3ff603e6852d7d6dbe
DNT-Khoa <khoa.coder.jedi@gmail.com>
2026-05-23 19:20:56 +0200

    ✨ feat(codeforces/283911c): add binary search solution

---
+++ codeforces/283911C/Solution.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * <b>Time complexity:</b> O(N + K + KLog(N))
+ *
+ * <p><b>Space complexity:</b> O(N + K)
+ *
+ * <p><b>Notes:</b>
+ * <ul>
+ *   <li></li>
+ * </ul>
+ */
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        
+        // read inputs
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int[] targets = new int[K];
+        for (int i = 0; i < K; i++) {
+            targets[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // loop through each targets/queries and do binary search
+        for (int target : targets) {
+            int left = -1, right = N;
+            while (right - left > 1) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] >= target) {
+                    right = mid;
+                } else {
+                    left = mid;
+                }
+            }
+            pw.println(right + 1);
+        }
+
+        pw.close();
+    }
+}
---
+++ codeforces/283911C/notes.md
@@ -0,0 +1,4 @@
+# 283911C
+
+[Problem](https://codeforces.com/edu/course/2/lesson/6/1/practice/contest/283911/problem/C)
+

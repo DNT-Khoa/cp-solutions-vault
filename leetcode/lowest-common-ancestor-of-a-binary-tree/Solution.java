43ce595eeab3dd9c26d46d929de4b0901797e514
DNT-Khoa <khoa.coder.jedi@gmail.com>
2026-07-11 19:35:41 +0200

    ✨ feat(leetcode): add lowest common ancestor of a binary tree

---
+++ leetcode/lowest-common-ancestor-of-a-binary-tree/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/**
+ * <b>Time complexity:</b> O(n) where n is the number of nodes in root
+ *
+ * <p><b>Space complexity:</b> O(h) where h is the height of root
+ *
+ * <p><b>Notes:</b>
+ * <ul>
+ *   <li></li>
+ * </ul>
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root.val == p.val || root.val == q.val) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if (left != null && right != null) return root;
+        return left != null ? left : right;
+    }
+}
---
+++ leetcode/lowest-common-ancestor-of-a-binary-tree/notes.md
@@ -0,0 +1,4 @@
+# lowest-common-ancestor-of-a-binary-tree
+
+[Problem](https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/)
+
